ba5f4474aad10c8ba15466fc95f889b35b16a537
wibu009 <80740282+wibu009@users.noreply.github.com>
2021-11-09 23:08:28 +0700

    Revert "Revert "create entties and database context""
    This reverts commit 106c433f0496c95d3f138d3344c12f5d7c4aa7d2.

---
+++ 1_DAL/Entities/ChiTietThietBi.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_DAL.Entities
+{
+    [Table("ChiTietThietBi")]
+    public class ChiTietThietBi
+    {
+        [Key]
+        [Column("IDPhong")]
+        [StringLength(20)]
+        public string Idphong { get; set; }
+        [Key]
+        [Column("IDMaTB")]
+        [StringLength(30)]
+        public string IdmaTb { get; set; }
+        public double? DonGia { get; set; }
+        public int? SoLuong { get; set; }
+        [Column("IDTranngThai")]
+        public int? IdtranngThai { get; set; }
+
+        [ForeignKey(nameof(IdmaTb))]
+        [InverseProperty(nameof(ThietBi.ChiTietThietBis))]
+        public virtual ThietBi IdmaTbNavigation { get; set; }
+        [ForeignKey(nameof(Idphong))]
+        [InverseProperty(nameof(Phong.ChiTietThietBis))]
+        public virtual Phong IdphongNavigation { get; set; }
+    }
+}
